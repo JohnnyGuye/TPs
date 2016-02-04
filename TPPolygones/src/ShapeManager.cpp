19c4094eaf6eb4e72bf107b4ee31b35a19820d0c
pngrdr <lucas.okumura@gmail.com>
2016-02-04 13:22:44 +0100

    load and store

--- TPPolygones/src/ShapeManager.cpp
+++ TPPolygones/src/ShapeManager.cpp
@@ -129,14 +129,14 @@ bool ShapeManager::Store(string const& fileName)
 {
 	ofstream storeFile(fileName.c_str());
 	ifstream ifile(fileName);
-	if (ifile.fail())
+	if (ifile.good())
 	{
 		return Answer("File already exists");
 	}
 	if(storeFile)
 	{
-		/*
 		ShapeMap::iterator shapeIt = ShapeTable.begin();
+		string compose="";
 		while(shapeIt != ShapeTable.end())
 		{
 			Shape* tmp = shapeIt->second;
@@ -144,31 +144,271 @@ bool ShapeManager::Store(string const& fileName)
 			switch(tmp->GetType())
 			{
 			case Shape::SEGMENT:
-				details = "S" + " " + tmp->GetName();
-				for(Vector2D vect : tmp->GetPositions)
-					details += " " + vect.GetX() + " " + vect.GetY();
+				details=tmp->GetPersistence();
+				break;
 			case Shape::RECTANGLE:
-				details = "R "+tmp->GetName();
-				for(Vector2D vect : tmp->GetPositions())
-					details +=" "+vect.GetX()+ " " + vect.GetY();
+				details=tmp->GetPersistence();
+				break;
 			case Shape::POLYCONV:
-				details = "PC "+tmp->GetName();
-				for(Vector2D vect : tmp->GetPositions())
-					details += " " + vect.GetX() + " " + vect.GetY();
+                details=tmp->GetPersistence();
+                break;
 			case Shape::REUSHAPE:
+			    compose+=tmp->GetPersistence()+"\r\n";
+			    break;
 			case Shape::INTERSHAPE:
+			    compose+=tmp->GetPersistence()+"\r\n";
+			    break;
 			case Shape::NOT_SHAPE:
+			    break;
 			}
+			storeFile<<details<<"\r\n";
 		}
-		*/
+		storeFile<<compose;
 	} else
 	{
 		return Answer("Unable to create file",false);
 	}
 	return Answer("The shapes are now stored in the file " + fileName,true);
 }
+//il faut envoyer shapesAdded a un LoadAll et l'ajouter dans History
+bool ShapeManager::Load(string fileName){
+    std::ifstream loadFile(fileName.c_str());
+    if(loadFile.fail())
+    {
+        return Answer("File does not exist",false);
+    }
+    string line;
+    vector<string> tokens;
+    vector<Shape *> shapesAdded;
+    vector<string> retry;
+	while(getline(loadFile, line))
+    {
 
-bool ShapeManager::Load(string const& fileName){
+        split(line, tokens);
+        if(tokens.size() > 0)
+        {
+            string cmd = tokens[0];
+
+            if(cmd == "S")//SEGMENT
+            {
+                try
+                {
+                    vector<Vector2D> points;
+                    if(tokens.size() != 6)	throw exception();
+
+                    for(unsigned int i = 2; i < tokens.size(); i+=2)
+                        points.push_back(Vector2D(my_stoi(tokens[i]),my_stoi(tokens[i+1])));
+
+                    Segment* seg = new Segment(tokens[1], points[0], points[1]);
+                    if(Add(seg))
+                        shapesAdded.push_back(seg);
+                    else
+                        delete seg;
+                }
+                catch(exception e)
+                {
+                    Answer("Invalide syntax. Here is the truth: ");
+                    cout << "S name x y x' y'";
+                }
+            }
+            else if(cmd == "R")//RECTANGLE
+            {
+                try
+                {
+                    vector<Vector2D> points;
+                    if(tokens.size()%2 != 6)	throw exception();
+
+                    for(unsigned int i = 2; i < tokens.size(); i+=2)
+                        points.push_back(Vector2D(my_stoi(tokens[i]),my_stoi(tokens[i+1])));
+
+                    Rectangle* rec = new Rectangle(tokens[1], points[0], points[1]);
+                    if(Add(rec))
+                        shapesAdded.push_back(rec);
+                    else
+                        delete rec;
+                }
+                catch(exception e)
+                {
+                    Answer("Invalide syntax. Here is the truth: ");
+                    cout << "R name x y x' y'";
+                }
+            }
+            else if(cmd == "PC")
+            {
+                try
+                {
+                    vector<Vector2D> points;
+                    if(tokens.size()%2 == 1 || tokens.size() < 8)	throw exception();
+                    for(unsigned int i = 2; i < tokens.size(); i+=2)
+                        points.push_back(Vector2D(my_stoi(tokens[i]),my_stoi(tokens[i+1])));
+
+                    if(PolyConv::VerifyConvexity(points))
+                    {
+                        PolyConv* pc = new PolyConv(tokens[1], points);
+                        if(Add(pc))
+                            shapesAdded.push_back(pc);
+                        else
+                            delete pc;
+                    }
+                    else
+                    {
+                        Answer("Not a convex polygone :/ If you want to design this, try to unify more single shapes");
+                    }
+                }
+                catch(exception e)
+                {
+                    Answer("Invalide syntax. Here is the truth: ");
+                    cout << "PC name x0 y0 x1 y1 ... xn yn";
+                }
+            }
+            else if(cmd == "OI")
+            {
+                try
+                {
+                    vector<string> notInTable;
+                    for(unsigned int i = 2; i < tokens.size(); i++)
+                        if(IsInTable(tokens[i]))
+                            notInTable.push_back(tokens[i]);
+
+                    if(notInTable.size() == 0)
+                    {
+                        vector<Shape*> shapes1;
+                        for(unsigned int i = 2; i < tokens.size(); i++)
+                            shapes1.push_back(findShape(tokens[i])->Clone());
+
+                        PolyIntersect* pi = new PolyIntersect(tokens[1], shapes1);
+                        if(Add(pi))
+                            shapesAdded.push_back(pi);
+                        else
+                            delete pi;
+
+                    }
+                    else
+                    {
+                        retry.push_back(line);
+                    }
+
+                }
+                catch(exception e)
+                {
+                    Answer("Invalid syntax, here lies the truth: ");
+                    Answer("OI name name0 name1 ... namen");
+                }
+            }
+            else if(cmd == "OR")
+            {
+                try
+                {
+                    vector<string> notInTable;
+                    for(unsigned int i = 2; i < tokens.size(); i++)
+                        if(IsInTable(tokens[i]))
+                            notInTable.push_back(tokens[i]);
+
+                    if(notInTable.size() == 0)
+                    {
+                        vector<Shape*> shapes1;
+                        for(unsigned int i = 2; i < tokens.size(); i++)
+                            shapes1.push_back(findShape(tokens[i])->Clone());
+
+                        PolyUnion* pu = new PolyUnion(tokens[1], shapes1);
+                        if(Add(pu))
+                           shapesAdded.push_back(pu);
+                        else
+                            delete pu;
+
+                    }
+                    else
+                    {
+                        retry.push_back(line);
+                    }
+
+                }
+                catch(exception e)
+                {
+                    Answer("Invalid syntax, here lies the truth: ");
+                    Answer("OI name name0 name1 ... namen");
+                }
+            }
+        }
+    }
+    bool proceed = retry.size()!=0;
+    while(proceed)
+    {
+        proceed=false;
+        for(string command:retry)
+        {
+             split(command, tokens);
+             string cmd = tokens[0];
+             if(cmd == "OI")
+            {
+                try
+                {
+                    vector<string> notInTable;
+                    for(unsigned int i = 2; i < tokens.size(); i++)
+                        if(IsInTable(tokens[i]))
+                            notInTable.push_back(tokens[i]);
+
+                    if(notInTable.size() == 0)
+                    {
+                        vector<Shape*> shapes1;
+                        for(unsigned int i = 2; i < tokens.size(); i++)
+                            shapes1.push_back(findShape(tokens[i])->Clone());
+
+                        PolyIntersect* pi = new PolyIntersect(tokens[1], shapes1);
+                        if(Add(pi))
+                        {
+                            shapesAdded.push_back(pi);
+                            proceed=true;
+                        }
+                        else
+                            delete pi;
+
+                    }
+
+                }
+                catch(exception e)
+                {
+                    Answer("Invalid syntax, here lies the truth: ");
+                    Answer("OI name name0 name1 ... namen");
+                }
+            }
+            else if(cmd == "OR")
+            {
+                try
+                {
+                    vector<string> notInTable;
+                    for(unsigned int i = 2; i < tokens.size(); i++)
+                        if(IsInTable(tokens[i]))
+                            notInTable.push_back(tokens[i]);
+
+                    if(notInTable.size() == 0)
+                    {
+                        vector<Shape*> shapes1;
+                        for(unsigned int i = 2; i < tokens.size(); i++)
+                            shapes1.push_back(findShape(tokens[i])->Clone());
+
+                        PolyUnion* pu = new PolyUnion(tokens[1], shapes1);
+                        if(Add(pu))
+                        {
+                           shapesAdded.push_back(pu);
+                           proceed=true;
+                        }
+                        else
+                            delete pu;
+
+                    }
+
+                }
+                catch(exception e)
+                {
+                    Answer("Invalid syntax, here lies the truth: ");
+                    Answer("OI name name0 name1 ... namen");
+                }
+            }
+        }
+    }
+    if(retry.size()!=0){
+        return Answer("Error loading shapes");
+    }
 	return Answer("The shapes have been copied into the local context",true);
 }
-
